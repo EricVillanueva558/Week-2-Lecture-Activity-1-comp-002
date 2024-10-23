eeee2b9f1fe6a514b1bfdd10b671a511d05fbbb0
Eric Villanueva <villanuevaeric1305@gmail.com>
2024-10-22 21:49:05 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("8 * null =", 8 * null); // Multiplying a number by null
+console.log('"5" - 1 =', "5" - 1); // Subtracting a number from a string
+console.log('"5" + 1 =', "5" + 1); // Adding a number to a string
+console.log('"five" * 2 =', "five" * 2); // Multiplying a string that is not a number
+console.log('false == 0 =', false == 0); // Comparing false with 0
+console.log('5 > 2 && 6 > 3 =', 5 > 2 && 6 > 3); // Logical AND between two conditions
+console.log('false == false ? "equal" : "not equal" =', false == false ? "equal" : "not equal"); // Ternary operator check
+console.log('true || false =', true || false); // Logical OR between true and false
